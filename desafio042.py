2de6b437faebad8982116a1fe926ae3de056914f
anagfc <116309863+anagfc@users.noreply.github.com>
2023-01-13 20:38:37 -0300

    update desafio 42

--- desafio042.py
+++ desafio042.py
@@ -5,13 +5,12 @@ l3 = float(input('Insira o 3º lado do triângulo: '))
 
 if l1 + l2 > l3 and l2 + l3 > l1 and l3 + l1 > l2:
     print('Essas medidas formam um triângulo.')
+    if l1 != l2 and l1 != l3:
+        print('Seu triângulo é escaleno.')
+    elif l1 == l2 and l1 == l3:
+        print('Seu triângulo é equilátero.')
+    else:
+        print('Seu triêngulo é isóceles.')
 else:
     print('Essas medidas NÃO formam um triângulo.')
-
-if l1 != l2 != l3:
-    print('Seu triângulo é escaleno.')
-elif l1 == l2 == l3:
-    print('Seu triângulo é equilátero.')
-else:
-    print('Seu triêngulo é isóceles.')
     
