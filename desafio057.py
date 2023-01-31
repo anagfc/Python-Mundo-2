ee53845ffc445a20f5b7d982c3aacede6e499959
anagfc <116309863+anagfc@users.noreply.github.com>
2023-01-31 15:53:16 -0300

    update desafio 57

--- desafio057.py
+++ desafio057.py
@@ -1,4 +1,4 @@
-sexo = input('Insira seu sexo [F/M]: ').upper()
-while sexo != 'F' and sexo != 'M':
+sexo = input('Insira seu sexo [F/M]: ').upper().strip()
+while sexo != 'F' and sexo != 'M': #While sexo not in 'MmFf':
     print('Resposta inválida.')
     sexo = input('Insira seu sexo [F/M]: ').upper()
